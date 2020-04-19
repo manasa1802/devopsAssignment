3dc7850bbf1baaba0435d35a52180d20281a14fc
manasa tr <manasatr1802@gmail.com>
2020-04-19 16:51:40 -0700

    this is the first commit

---
+++ devops01a.sh
@@ -0,0 +1,18 @@
+this is my first bash script
+              disk0       cpu    load average
+    KB/t  tps  MB/s  us sy id   1m   5m   15m
+   71.90   57  3.98   7  7 86  1.39 1.58 1.50
+number of volumes, size of each volume, free space on each volume
+Filesystem    512-blocks      Used Available Capacity iused      ifree %iused  Mounted on
+/dev/disk1s5   236363688  21187680  55960672    28%  484116 1181334324    0%   /
+devfs                396       396         0   100%     700          0  100%   /dev
+/dev/disk1s1   236363688 153578064  55960672    74% 1555042 1180263398    0%   /System/Volumes/Data
+/dev/disk1s4   236363688   4194360  55960672     7%       2 1181818438    0%   /private/var/vm
+map auto_home          0         0         0   100%       0          0  100%   /System/Volumes/Data/home
+the IP address of my machine
+? (192.168.0.1) at 3c:37:86:2:5e:8a on en0 ifscope [ethernet]
+? (192.168.0.10) at 0:18:4d:ff:ff:7 on en0 ifscope [ethernet]
+? (192.168.0.12) at 0:4:4b:71:e1:e4 on en0 ifscope [ethernet]
+? (192.168.0.22) at 48:8d:36:96:71:51 on en0 ifscope [ethernet]
+? (224.0.0.251) at 1:0:5e:0:0:fb on en0 ifscope permanent [ethernet]
+? (239.255.255.250) at 1:0:5e:7f:ff:fa on en0 ifscope permanent [ethernet]
